ee3f56bbbf77c79ab259aaef38e98050de838ad4
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2023-02-27 17:43:56 -0800

    Sets up a controller class with routing, bumps version

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,16 +2,40 @@
 
 require_relative "rulers/version"
 require 'rulers/array'
+require 'rulers/routing'
 
 module Rulers
   class Application
     def call(env)
       `echo debug > debug.txt`;
+
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [
+          404,
+          { 'Content-Type' => 'text/html' },
+          []
+        ]
+      end
+
+      klass, action = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(action)
+
       [
         200,
         {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers"]
+        [text]
       ]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
